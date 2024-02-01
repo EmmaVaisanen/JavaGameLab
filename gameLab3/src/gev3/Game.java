3722794b83f96014dd4c5eec54f7cfc0c4d1aa17
EmmaVaisanen <emmavaisse@outlook.com>
2024-02-01 16:24:03 +0100

    Added dimensions, draw-functions, keyboard update support for Ball and Bat classes.

--- gameLab3/src/gev3/Game.java
+++ gameLab3/src/gev3/Game.java
@@ -4,15 +4,22 @@ import java.awt.Graphics2D;
 import java.util.*;
 
 public class Game {
+	private Ball circle;
+	private Bat bat;
+	private ArrayList<Ball> ball;
 	public Game(GameBoard board) {
-
+		circle = new Ball(350, 100, 40, 40);
+		bat = new Bat(300, 550,150,20, Color.LIGHT_GRAY);
+		ball = new ArrayList<>();
 	}
 
 	public void update(Keyboard keyboard) {
-
+		circle.update(keyboard);
+		bat.update(keyboard);
 	}
 
 	public void draw(Graphics2D graphics) {
-
+		circle.draw(graphics);
+		bat.draw(graphics);
 	}
 }
