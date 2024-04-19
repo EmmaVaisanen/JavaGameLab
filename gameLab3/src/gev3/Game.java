7efa8e7be4d9928de333691451fd64f892136614
EmmaVaisanen <emmavaisse@outlook.com>
2024-04-19 18:55:02 +0200

    Added new methods for game states, collision logics, draw methods. Updated constructor in class.

--- gameLab3/src/gev3/Game.java
+++ gameLab3/src/gev3/Game.java
@@ -1,42 +1,116 @@
 package gev3;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.util.*;
-import java.awt.Rectangle;
+
+import java.awt.*;
+import java.util.ArrayList;
 
 public class Game {
-	private Ball circle;
-	private Bat bat;
-	private Bricks brick;
-	private ArrayList<Ball> ball;
-	private boolean checkCollision(Ball circle, Bat bat) {
-		Rectangle circleRect = new Rectangle(circle.getX(), circle.getY(), circle.getWidth(), circle.getHeight());
-		Rectangle batRect = new Rectangle(bat.getX(), bat.getY(), bat.getWidth(), bat.getHeight());
+    // Initieringen av lite krimskrams.
+    private Ball circle;
+    private Bat bat;
+    private ArrayList<Sprite> scaryMonstersAndNiceSprites; // Sprites för block och väggar
+    private ArrayList<Brickwall> bricks;
+    private ArrayList<Ball> ball;
+    private int score;
+    private GameBoard gameBoard;
+    private boolean isGameOver = false; // Initierar game over och förklarar det falsk vid start under programmet
+    private ArrayList<Wall> walls;
+    public int getScore() {
+        return score;
+    }
 
-		return circleRect.intersects(batRect);
-	}
-
-	public Game(GameBoard board) {
-		circle = new Ball(350, 100, 40, 40, 0, 2);
-		bat = new Bat(300, 550,150,20, Color.LIGHT_GRAY);
-		ball = new ArrayList<>();
-		brick = new Bricks(50, 50, 400, 200, 3, 5);
-	}
-
-	public void update(Keyboard keyboard) {
-		circle.update(keyboard);
-		bat.update(keyboard);
-		brick.update(keyboard);
-
-		if(checkCollision(circle, bat, brick)){
-			circle.setDy(-circle.getDy());
-		}
-	}
+    // Konstruktor som initierar variabler och spelet.
+    public Game(GameBoard board) {
+        scaryMonstersAndNiceSprites = new ArrayList<>();
+        circle = new Ball(Const.BALL_SPAWNX, Const.BALL_SPAWNY, Const.BALL_SIZE, Const.BALL_SIZE, Const.BALL_SPEEDX, -(Const.BALL_SPEEDY), Const.BALL_LIVES, this);
+        bat = new Bat(Const.BAT_POSX, Const.BAT_POSY, Const.BAT_WIDTH, Const.BAT_HEIGHT, Color.LIGHT_GRAY, "EV", Color.BLACK);
+        ball = new ArrayList<>();
+        bricks = new ArrayList<>();
+        walls = new ArrayList<>();
+        score = 0;
+        gameBoard = board;
 
 
-	public void draw(Graphics2D graphics) {
-		circle.draw(graphics);
-		bat.draw(graphics);
-		brick.draw(graphics);
-	}
+        Brickwall brick = new Brickwall(0, Const.BRICKWALL_POSY, Const.BRICKWALL_WIDTH, Const.BRICKWALL_HEIGHT, Const.NUM_ROWS, Const.NUM_COLS, this);
+        bricks.add(brick);
+        for (Block block : brick.getBlocks()) {
+            scaryMonstersAndNiceSprites.add(block);
+        }
+
+        Wall westWall = new Wall(0, 0, Const.WALL_WIDTH, Const.WALL_HEIGHT);
+        Wall eastWall = new Wall(board.getPreferredSize().width - Const.WALL_WIDTH, 0, Const.WALL_WIDTH, board.getPreferredSize().height);
+        Wall northWall = new Wall(0, 0, board.getPreferredSize().width, Const.WALL_NORTHHEIGHT);
+
+        walls.add(westWall);
+        walls.add(eastWall);
+        walls.add(northWall);
+
+        scaryMonstersAndNiceSprites.add(westWall);
+        scaryMonstersAndNiceSprites.add(eastWall);
+        scaryMonstersAndNiceSprites.add(northWall);
+
+        ball.add(circle);
+    }
+
+    // Sätter game over till true
+    public void gameOver() {
+        isGameOver = true;
+    }
+
+    // Returnerar game over till hela programmet
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void updateScore(int points, int lives) {
+        score += points;
+        gameBoard.updateStatus(score, lives); // Används för att uppdatera statusen på poäng och liv.
+    }
+
+    // Uppdaterar spelets logik och olika tillstånd
+    public void update(Keyboard keyboard) {
+        for (Sprite sp : scaryMonstersAndNiceSprites) {
+            sp.update(keyboard);
+        }
+
+        for (Sprite sp : ball) {
+            sp.update(keyboard);
+            if (sp instanceof Ball) {
+                Ball ball = (Ball) sp;
+                if (bat.getBounds().intersects(ball.getBounds())) {
+                    ball.setDy(-ball.getDy());
+                }
+                for (Wall wall : walls) {
+                    if (ball.getBounds().intersects(wall.getBounds())) {
+                        wall.collisionHandling(ball);
+                    }
+                }
+                if (isGameOver) {
+                    return;
+                }
+            }
+        }
+
+        for (Brickwall brickwall : bricks) {
+            brickwall.update(keyboard);
+            brickwall.collisionHandling(ball);
+            for (Block block : brickwall.getBlocks()) {
+                block.getColor();
+            }
+        }
+        bat.update(keyboard);
+    }
+
+    // Ritar ut objekten i spelet
+    public void draw(Graphics2D graphics) {
+        circle.draw(graphics);
+        bat.draw(graphics);
+
+        for (Brickwall block : bricks) {
+            block.draw(graphics);
+        }
+
+        for (Sprite sprite : scaryMonstersAndNiceSprites) {
+            sprite.draw(graphics);
+        }
+    }
 }
